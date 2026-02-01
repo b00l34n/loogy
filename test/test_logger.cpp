bcb15fe37ac0d9e9c9c665691d19cc985d3b1ded
So[c]ke <a.sokol@hawksracing.de>
2026-02-01 16:58:50 +0100

    [test] basic tests work

--- test/test_logger.cpp
+++ test/test_logger.cpp
@@ -20,7 +20,7 @@ void setUp(void)
 void tearDown(void) {}
 
 
-void test_init ( void ) 
+void init ( void ) 
 {
 	logger dut = logger(DUT_RESULT_LOG);
 	dut.~logger();
@@ -29,7 +29,8 @@ void test_init ( void )
 
 }
 
-void test_info ( void ) 
+
+void info ( void ) 
 {	
 	const char * test_str = "test";
 	logger dut = logger(DUT_RESULT_LOG);
@@ -41,7 +42,7 @@ void test_info ( void )
 	str_msg += test_str;
 	str_msg += ")";
 	const std::regex regx_msg(str_msg);
-	const std::regex regx_type("(INFO)");
+	const std::regex regx_type(R"(^INFO.*)");
 
 	FILE *	fp;
 	char *	line = NULL;
@@ -51,35 +52,31 @@ void test_info ( void )
 
 	fp = fopen(DUT_RESULT_LOG, "r");
 	TEST_ASSERT_MESSAGE (fp != NULL, "failed to open created log file for analyses");
-	do 
+		
+	read = getline(&line, &len, fp);
+	TEST_ASSERT_MESSAGE ( read != -1, "log file is empty");	
+	while ( read != -1 ) 
 	{
-		read = getline(&line, &len, fp);
-		fprintf(stderr, "%s", line);
-		fprintf
-		(
-			stderr,
-			"%ld | %ld\n",
-			read,
-			(INFO_MSG_BASE_SIZE + result_string.getString().size())
-		);
 		TEST_ASSERT_MESSAGE
 		(
-				read == (INFO_MSG_BASE_SIZE + result_string.getString().size()),
+				read == result_string.toString().size(),
 				"line length missmatch"
 		);
 		TEST_ASSERT_MESSAGE
 		(
-		 		std::regex_match(line, regx_type),
+		 		std::regex_search(line, regx_type),
 				"msg type missmatch"
 		);
 		TEST_ASSERT_MESSAGE
 		(
-		 		std::regex_match(line, regx_msg),
+		 		std::regex_search(line, regx_msg),
 				"msg value missmatch"
 		);
 
+		
 		++line_count;
-	} while ( read != -1 );
+		read = getline(&line, &len, fp);
+	}
 	fclose(fp);
 	
 	TEST_ASSERT_MESSAGE
@@ -90,13 +87,133 @@ void test_info ( void )
 
 }
 
+
+void warn ( void ) 
+{	
+	const char * test_str = "warn";
+	logger dut = logger(DUT_RESULT_LOG);
+	dut.warn(test_str);	
+	dut.~logger();
+
+	logMessage result_string = logMessage(_e_warn, test_str); 
+	std::string str_msg = "(";
+	str_msg += test_str;
+	str_msg += ")";
+	const std::regex regx_msg(str_msg);
+	const std::regex regx_type(R"(^WARN.*)");
+
+	FILE *	fp;
+	char *	line = NULL;
+	size_t	len = 0;
+	ssize_t	read = 0;
+	size_t	line_count = 0;
+
+	fp = fopen(DUT_RESULT_LOG, "r");
+	TEST_ASSERT_MESSAGE (fp != NULL, "failed to open created log file for analyses");
+		
+	read = getline(&line, &len, fp);
+	TEST_ASSERT_MESSAGE ( read != -1, "log file is empty");	
+	while ( read != -1 ) 
+	{
+		TEST_ASSERT_MESSAGE
+		(
+				read == result_string.toString().size(),
+				"line length missmatch"
+		);
+		TEST_ASSERT_MESSAGE
+		(
+		 		std::regex_search(line, regx_type),
+				"msg type missmatch"
+		);
+		TEST_ASSERT_MESSAGE
+		(
+		 		std::regex_search(line, regx_msg),
+				"msg value missmatch"
+		);
+
+		
+		++line_count;
+		read = getline(&line, &len, fp);
+	}
+	fclose(fp);
+	
+	TEST_ASSERT_MESSAGE
+	(
+		line_count == 1,
+		"line count missmatch"
+	);
+
+}
+
+
+void error ( void ) 
+{	
+	const char * test_str = "error";
+	logger dut = logger(DUT_RESULT_LOG);
+	dut.error(test_str);	
+	dut.~logger();
+
+	logMessage result_string = logMessage(_e_error, test_str); 
+	std::string str_msg = "(";
+	str_msg += test_str;
+	str_msg += ")";
+	const std::regex regx_msg(str_msg);
+	const std::regex regx_type(R"(^ERROR.*)");
+
+	FILE *	fp;
+	char *	line = NULL;
+	size_t	len = 0;
+	ssize_t	read = 0;
+	size_t	line_count = 0;
+
+	fp = fopen(DUT_RESULT_LOG, "r");
+	TEST_ASSERT_MESSAGE (fp != NULL, "failed to open created log file for analyses");
+		
+	read = getline(&line, &len, fp);
+	TEST_ASSERT_MESSAGE ( read != -1, "log file is empty");	
+	while ( read != -1 ) 
+	{
+		TEST_ASSERT_MESSAGE
+		(
+				read == result_string.toString().size(),
+				"line length missmatch"
+		);
+		TEST_ASSERT_MESSAGE
+		(
+		 		std::regex_search(line, regx_type),
+				"msg type missmatch"
+		);
+		TEST_ASSERT_MESSAGE
+		(
+		 		std::regex_search(line, regx_msg),
+				"msg value missmatch"
+		);
+
+		
+		++line_count;
+		read = getline(&line, &len, fp);
+	}
+	fclose(fp);
+	
+	TEST_ASSERT_MESSAGE
+	(
+		line_count == 1,
+		"line count missmatch"
+	);
+
+}
+
+
+
 int main ( void )
 {
 
 	UNITY_BEGIN();
 
-	RUN_TEST( test_init );
-	RUN_TEST( test_info );
+	RUN_TEST( init );
+	RUN_TEST( info );
+	RUN_TEST( warn );
+	RUN_TEST( error );
 
 	return UNITY_END(); 
 
